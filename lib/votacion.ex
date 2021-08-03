36ac947a69763de0189a8455089231010437a1ad
Enrique Leigh <pipo.leigh@gmail.com>
2021-08-03 10:01:25 -0400

    functional core

--- lib/votacion.ex
+++ lib/votacion.ex
@@ -1,18 +1,19 @@
 defmodule Votacion do
-  @moduledoc """
-  Documentation for `Votacion`.
-  """
+    def jugadores() do
+    jugadores = %{
+        :enrique => 0,
+        :agustin => 0,
+        :victoria => 0
+      }
+    end
 
-  @doc """
-  Hello world.
+    def get_score(jugador) do
+      Votacion.jugadores |>
+      Map.get(jugador)
+    end
 
-  ## Examples
-
-      iex> Votacion.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
+    def update_score(jugador) do
+      Votacion.jugadores |>
+      Map.put(jugador, Votacion.get_score(jugador)+1)
+    end
 end
