9f4f806736d63aeb6f81e5aa9f08809ba6dfef44
Enrique Leigh <pipo.leigh@gmail.com>
2022-02-01 12:43:31 -0300

    bugfixes del contexto

--- lib/voter/boundary/game_server.ex
+++ lib/voter/boundary/game_server.ex
@@ -3,8 +3,8 @@ defmodule Voter.GameServer do
   alias Voter.Core.Game
   
   # Función de API	
-  def start do
-    GenServer.start(__MODULE__, nil)
+  def start(params) do
+    GenServer.start(__MODULE__, params)
   end
 
   def add_player(game_server, player) do
@@ -15,8 +15,8 @@ defmodule Voter.GameServer do
     GenServer.call(game_server, {:add_vote, vote})
   end
 
-  def game_stats(game_server) do
-    GenServer.call(game_server, {:stats})
+  def view_game(game_server) do
+    GenServer.call(game_server, {:view})
   end
 
   def select_winner(game_server) do
@@ -40,35 +40,35 @@ defmodule Voter.GameServer do
   @impl true
   def handle_call({:add_player, player}, _from, game) do
     new_game = Game.add_player(game, player)
-    {:reply, new_game}
+    {:reply, new_game, new_game}
   end
 
   @impl true
   def handle_call({:add_vote, vote}, _from, game) do
     new_game = Game.add_vote(game, vote)
-    {:reply, new_game}
+    {:reply, new_game, new_game}
   end
 
   @impl true
   def handle_call({:select_winner}, _from, game) do
     new_game = Game.select_winner(game)
-    {:reply, new_game}
+    {:reply, new_game, new_game}
   end
 
   @impl true
   def handle_call({:view}, _from, game) do
-    {:reply, game}
+    {:reply, game, game}
   end
 
   @impl GenServer
   def handle_call({:activate}, _from, game) do
     new_game = Game.activate(game)
-    {:reply, new_game}
+    {:reply, new_game, new_game}
   end
 
   @impl GenServer
   def handle_call({:close}, _from, game) do
     new_game = Game.close(game)
-    {:reply, new_game}
+    {:reply, new_game, new_game}
   end
 end
--- lib/voter/context.ex
+++ lib/voter/context.ex
@@ -1,26 +1,27 @@
 defmodule Voter do
   alias Voter.GameServer
 
-  def new_game() do
-    GameServer.start()
+  def new_game(params) do
+    GameServer.start(params)
   end
 
   def add_player(game, name) do
-    game.add_player(name)
+    GameServer.add_player(game, name)
   end
 
   def add_vote(game, _email, player_name) do
-    game.add_vote(game, player_name)
+    GameServer.add_vote(game, player_name)
   end
 
   def activate(game) do
-    game.activate()
+    GameServer.activate()
   end
 
   def close(game) do
-    game.close()
+    GameServer.close()
   end
-
+  
+  def view_game(game), do: GameServer.view_game(game)
 #  def select_vote_from(email) do
 #    Game.select_vote_from(email)
 #  end
